e7d829e39b5ea596ac89579b78a303d1c09b1749
grs-ann <66564854+grs-ann@users.noreply.github.com>
2020-09-11 12:34:37 +0400

    ColumnFinish class, ColumnFinish Form has been adding.
    Добавил сам класс и форму для отделки, но нужно ещё это все увязать с Application class.

---
+++ TerrTools/UI/ColumnFinish.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TerrTools.UI
+{
+    public partial class ColumnFinish : Form
+    {
+        public ColumnFinish()
+        {
+            InitializeComponent();
+        }
+    }
+}
