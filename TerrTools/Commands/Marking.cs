dfb46a5a1d41951cf7483c3cff666ae663e30f51
grs-ann <66564854+grs-ann@users.noreply.github.com>
2020-12-04 16:29:12 +0400

    При создании учтены позиция марки, параметры выноски, исправлена ошибка с созданием пространств и помещений с типом, не соответствующим типу выбранной марки.

--- TerrTools/Commands/Marking.cs
+++ TerrTools/Commands/Marking.cs
@@ -28,7 +28,7 @@ namespace TerrTools
                 UIDoc = commandData.Application.ActiveUIDocument;
                 var selectedMarkReference = UIDoc.Selection.PickObject(ObjectType.Element, new TagsSelectionFilter(), "Выберите марку");
                 var selectedMark = Doc.GetElement(selectedMarkReference.ElementId);
-                MarkingElements(selectedMarkReference, selectedMark);
+                MarkingElements(selectedMark);
                 return Result.Succeeded;
             }
             catch (Autodesk.Revit.Exceptions.OperationCanceledException)
@@ -37,6 +37,8 @@ namespace TerrTools
             }
             
         }
+        // Возвращает все экземпляры, присущие категории выбранной
+        // марки на виде для их последующей маркировки.
         private IEnumerable<ElementId> GetAllViewInstancesByCategory(ElementId selectedItem)
         {
            var excessItem =  Doc.GetElement(selectedItem);
@@ -47,56 +49,135 @@ namespace TerrTools
                          .Where(x => excessItem.Id != x);
            return allViewInstanceIds;
         }
-        // Маркиру ет элементы с учетом свойств выбранной пользователем марки.
-        // нужно BIC а не mark
-        private void MarkingElements(Reference selectedMarkReference, Element selectedTag)
+        // Маркирует элементы с учетом свойств выбранной пользователем марки.
+        private void MarkingElements(Element tag)
         {
             using (Transaction trans = new Transaction(Doc))
             {
-                trans.Start("Creating new tags for elements");
+                trans.Start("Создание марок для элемента");
                 // Случай, когда пользователь выбрал марку помещения.
-                if ((BuiltInCategory)selectedTag.Category.Id.IntegerValue == BuiltInCategory.OST_RoomTags)
+                if ((BuiltInCategory)tag.Category.Id.IntegerValue == BuiltInCategory.OST_RoomTags)
                 {
-                    var selectedRoomTag = (RoomTag)selectedTag;
+                    var selectedRoomTag = (RoomTag)tag;   
                     var instancesIdsForTagging = GetAllViewInstancesByCategory(selectedRoomTag.Room.Id);
+                    var tagPosition = selectedRoomTag.TagHeadPosition;
+                    var TagHostCenterPoint = GetCenterFromBoundingBox(Doc.GetElement(selectedRoomTag.Room.Id));
                     foreach (var instanceIdForTagging in instancesIdsForTagging)
                     {
+                        var instanceForTagging = Doc.GetElement(instanceIdForTagging);
                         var tagEndPoint = GetCenterFromBoundingBox(Doc.GetElement(instanceIdForTagging));
                         UV uvPoint = new UV(tagEndPoint.X, tagEndPoint.Y);
                         var createdRoomTag = Doc.Create.NewRoomTag(new LinkElementId(instanceIdForTagging), uvPoint, UIDoc.ActiveView.Id);
+                        createdRoomTag.RoomTagType = selectedRoomTag.RoomTagType;
                         createdRoomTag.HasLeader = selectedRoomTag.HasLeader;
                         createdRoomTag.TagOrientation = selectedRoomTag.TagOrientation;
+                        // В случае, когда марка имеет выноску,
+                        // производится настройка выноски для созданной марки.
+                        if (selectedRoomTag.HasLeader)
+                        {
+                            var hostElementLocation = selectedRoomTag.Room.Location as LocationPoint;
+                            var hostLP = hostElementLocation.Point;
+                            var locationInstance = instanceForTagging.Location as LocationPoint;
+                            var instanceLP = locationInstance.Point;
+                            createdRoomTag.LeaderEnd = GetPointForOffset(hostLP, selectedRoomTag.LeaderEnd, instanceLP);
+                            createdRoomTag.TagHeadPosition = GetPointForOffset(selectedRoomTag.LeaderEnd, selectedRoomTag.TagHeadPosition, createdRoomTag.LeaderEnd);
+                            // Если выноска загибается.
+                            if (selectedRoomTag.HasElbow)
+                            {
+                                createdRoomTag.LeaderElbow = GetPointForOffset(selectedRoomTag.TagHeadPosition, selectedRoomTag.LeaderElbow, createdRoomTag.TagHeadPosition);
+                            }
+                        }
                     }
                 }
                 // Случай, когда пользователь выбрал марку пространства.
-                else if ((BuiltInCategory)selectedTag.Category.Id.IntegerValue == BuiltInCategory.OST_MEPSpaceTags)
+                else if ((BuiltInCategory)tag.Category.Id.IntegerValue == BuiltInCategory.OST_MEPSpaceTags)
                 {
-                    var selectedSpaceTag = (SpaceTag)selectedTag;
+                    var selectedSpaceTag = (SpaceTag)tag;
                     var instancesIdsForTagging = GetAllViewInstancesByCategory(selectedSpaceTag.Space.Id);
                     foreach (var instanceIdForTagging in instancesIdsForTagging)
                     {
+                        var instanceForTagging = Doc.GetElement(instanceIdForTagging);
                         var tagEndPoint = GetCenterFromBoundingBox(Doc.GetElement(instanceIdForTagging));
                         UV uvPoint = new UV(tagEndPoint.X, tagEndPoint.Y);
                         var createdSpaceTag = Doc.Create.NewSpaceTag((Space)Doc.GetElement(instanceIdForTagging), uvPoint, UIDoc.ActiveView);
+                        createdSpaceTag.SpaceTagType = selectedSpaceTag.SpaceTagType;
                         createdSpaceTag.TagOrientation = selectedSpaceTag.TagOrientation;
                         createdSpaceTag.HasLeader = selectedSpaceTag.HasLeader;
+                        // В случае, когда марка имеет выноску,
+                        // производится настройка выноски для созданной марки.
+                        if (selectedSpaceTag.HasLeader)
+                        {
+                            var hostElementLocation = selectedSpaceTag.Space.Location as LocationPoint;
+                            var hostLP = hostElementLocation.Point;
+                            var locationInstance = instanceForTagging.Location as LocationPoint;
+                            var instanceLP = locationInstance.Point;
+                            createdSpaceTag.LeaderEnd = GetPointForOffset(hostLP, selectedSpaceTag.LeaderEnd, instanceLP);
+                            createdSpaceTag.TagHeadPosition = GetPointForOffset(selectedSpaceTag.LeaderEnd, selectedSpaceTag.TagHeadPosition, createdSpaceTag.LeaderEnd);
+                            // Если выноска загибается.
+                            if (selectedSpaceTag.HasElbow)
+                            {
+                                createdSpaceTag.LeaderElbow = GetPointForOffset(selectedSpaceTag.TagHeadPosition, selectedSpaceTag.LeaderElbow, createdSpaceTag.TagHeadPosition);
+                            }
+                        }
                     }
                 }
                 else
                 {
-                    var independentSelectedTag = (IndependentTag)selectedTag;
-                    var instancesIdsForTagging = GetAllViewInstancesByCategory(independentSelectedTag.TaggedLocalElementId);
+                    var selectedTag = (IndependentTag)tag;
+                    var instancesIdsForTagging = GetAllViewInstancesByCategory(selectedTag.TaggedLocalElementId);
+                    var tagPosition= selectedTag.TagHeadPosition;
+                    var TagHostCenterPoint = GetCenterFromBoundingBox(Doc.GetElement(selectedTag.TaggedLocalElementId));
                     foreach (var instanceIdForTagging in instancesIdsForTagging)
                     {
-                        Reference tagHostReference = new Reference(Doc.GetElement(instanceIdForTagging));
-                        var tagEndPoint = GetCenterFromBoundingBox(Doc.GetElement(instanceIdForTagging));
-                        var createdTag = IndependentTag.Create(Doc, independentSelectedTag.GetTypeId(), UIDoc.ActiveView.Id,
-                        tagHostReference, independentSelectedTag.HasLeader, independentSelectedTag.TagOrientation, tagEndPoint);
-                        createdTag.LeaderEndCondition = independentSelectedTag.LeaderEndCondition;
+                        var instanceForTagging = Doc.GetElement(instanceIdForTagging);
+                        var creatingMarkHeaderPoint = GetPointForOffset(TagHostCenterPoint,
+                            tagPosition, GetCenterFromBoundingBox(instanceForTagging));
+                        Reference tagHostReference = new Reference(instanceForTagging);
+                        // Создается новый экземпляр марки.
+                        var createdTag = IndependentTag.Create(Doc, selectedTag.GetTypeId(), UIDoc.ActiveView.Id,
+                        tagHostReference, selectedTag.HasLeader, selectedTag.TagOrientation, creatingMarkHeaderPoint);
+                        createdTag.LeaderEndCondition = selectedTag.LeaderEndCondition;
+                        // В случае, когда марка имеет выноску,
+                        // производится настройка выноски для созданной марки.
+                        if (selectedTag.HasLeader)
+                        {
+                            var hostElementLocation = selectedTag.GetTaggedLocalElement().Location as LocationPoint;
+                            var hostLP = hostElementLocation.Point;
+                            var locationInstance = instanceForTagging.Location as LocationPoint;
+                            var instanceLP = locationInstance.Point;
+                            // Если выноска имеет закрепленный конец.
+                            if (selectedTag.LeaderEndCondition == LeaderEndCondition.Attached)
+                            {
+                                createdTag.TagHeadPosition = GetPointForOffset(hostLP, selectedTag.TagHeadPosition, instanceLP);
+                            }
+                            // Если выноска имеет свободный конец.
+                            else
+                            {
+                                createdTag.LeaderEnd = GetPointForOffset(hostLP, selectedTag.LeaderEnd, instanceLP);
+                                createdTag.TagHeadPosition = GetPointForOffset(selectedTag.LeaderEnd, selectedTag.TagHeadPosition, createdTag.LeaderEnd);
+                            }
+                            // Если выноска загибается.
+                            if (selectedTag.HasElbow)
+                            {
+                                createdTag.LeaderElbow = GetPointForOffset(selectedTag.TagHeadPosition, selectedTag.LeaderElbow, createdTag.TagHeadPosition);
+                            }
+                        }
                     }
                 }
                 trans.Commit();
             }
+        }
+        /// <summary>
+        /// Возвращает координаты точки, в которую нужно поместить
+        /// ту или иную часть выноски(end, header, elbow).
+        /// </summary>
+        /// <param name="pt1">Координаты начала вектора</param>
+        /// <param name="pt2">Координаты конца вектора</param>
+        /// <param name="pt3">Координаты начала другого вектора</param>
+        private XYZ GetPointForOffset(XYZ pt1, XYZ pt2, XYZ pt3)
+        {
+            var vector = pt2 - pt1;
+            return pt3 + vector;
         }
         // Возвращает центр из BoundingBox элемента.
         private XYZ GetCenterFromBoundingBox(Element el)
@@ -111,9 +192,12 @@ namespace TerrTools
         {
             public bool AllowElement(Element element)
             {
-                if (element.Category.IsTagCategory)
+                if (element != null)
                 {
-                    return true;
+                    if (element.Category.IsTagCategory)
+                    {
+                        return true;
+                    }
                 }
                 return false;
             }
